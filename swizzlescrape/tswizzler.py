149b8e61b9f6baa36432f076a2b49669f0a7f329
Edwin Zhang <ehzhang@mit.edu>
2015-01-17 23:04:06 -0500

    get swizzled

--- swizzlescrape/tswizzler.py
+++ swizzlescrape/tswizzler.py
@@ -1,13 +1,46 @@
-PATH_TO_SHAKE = 'videos/ShakeItOff.mp4'
+import random
 
 from moviepy.editor import VideoFileClip, concatenate
+from data import shake
 
-video = VideoFileClip("videos/ShakeItOff/ShakeItOff.mp4")
+videos = {
+    'shake': VideoFileClip('videos/ShakeItOff.mp4'),
+    'blank': VideoFileClip('videos/BlankSpace.mp4')
+}
+
+db = {}
+
+def build_db(name, words):
+    for item in words:
+        word, start, end = item
+        if word in db:
+            db[word].append([name, start, end])
+        else:
+            db[word] = [[name, start, end]]
+
+build_db('shake', shake.words)
 
 def make_video(word, start, end):
     video.subclip(start, end).to_videofile('words/' + word + '.mp4')
 
+def get_cuts(words):
+    cuts = []
+    for word in words:
+        if word in db:
+            cuts.append(random.choice(db[word]))
+    return cuts;
 
+def assemble_cuts(cuts, filename):
+    final = concatenate([videos[video].subclip(start, end)
+                         for (video, start, end) in cuts])
+    final.to_videofile(filename)
+
+def swizzle(sentence, output="swizzled.mp4"):
+    words = sentence.split()
+    cuts = get_cuts(words)
+    assemble_cuts(cuts, output)
+
+swizzle("hey baby")
 
 # make_video('dates', 18.025, 18.777)
 
