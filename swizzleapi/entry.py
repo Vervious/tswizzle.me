8bb93cce3cb6580cc43fea8c1d7bd2af4958dd5c
Benjamin Y Chan <me@vervious.com>
2015-01-18 05:13:27 -0500

    Basic caching

--- swizzleapi/entry.py
+++ swizzleapi/entry.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, hashlib
 # hack because relative import in python is hard to work around
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "swizzlescrape"))
 
@@ -18,9 +18,10 @@ def splash():
 @app.route("/swizzle", methods=['GET'])
 def get_swizzle():
     inputString = request.args['txt']
-    fileName = str(uuid.uuid4()) + '.mp4'
+    fileName = hashlib.md5(inputString.encode()).hexdigest() + '.mp4'
     newSwizzleURL = '../tempstore/' + fileName
-    T.swizzle(inputString, output=newSwizzleURL, hasText=True)
+    if os.path.isfile(newSwizzleURL) is False:
+        T.swizzle(inputString, output=newSwizzleURL, hasText=True)
     return app.send_static_file(fileName)
 
 # start it all
